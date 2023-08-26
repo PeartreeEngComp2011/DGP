dfb03ef457431c107df8064a3aca2ce539170b88
Daniel Gusmao <danielgp20@hotmail.com>
2023-08-26 12:53:03 -0300

    nova versão

--- fake-natty.php
+++ fake-natty.php
@@ -1,90 +1,119 @@
+<?php
+//Verificação para ver se o método de solicitação HTTP é POST
+if ($_SERVER["REQUEST_METHOD"] === "POST") 
+{
+    //Obter o número de grandes prêmios $G e número de pilotos $P
+    $G = isset($_POST["G"]) ? intval($_POST["G"]) : 0;
+    $P = isset($_POST["P"]) ? intval($_POST["P"]) : 0;
+
+    $pontuacao_atual = array_fill(1, $P, 0);
+    $S = isset($_POST["S"]) ? intval($_POST["S"]) : 0;
+    $campeoes = array();
+
+    for ($i = 0; $i < $G; $i++) 
+    {
+        $ordem_chegada = isset($_POST["corrida$i"]) ? $_POST["corrida$i"] : array();
+        for ($j = 0; $j < $P; $j++) 
+        {
+            $piloto = isset($ordem_chegada[$j]) ? intval($ordem_chegada[$j]) : 0;
+            $pontuacao_atual[$piloto] += $j;
+        }
+    }
+
+    for ($sistema = 0; $sistema < $S; $sistema++) 
+    {
+        $K = isset($_POST["K$sistema"]) ? intval($_POST["K$sistema"]) : 0;
+        $pontos = isset($_POST["pontos$sistema"]) ? $_POST["pontos$sistema"] : array();
+        for ($j = 0; $j < $K; $j++) 
+        {
+            $pontos[] = intval($_POST["pontos$sistema$j"]);
+        }
+
+        $pontuacao_total = array();
+        for ($piloto = 1; $piloto <= $P; $piloto++) 
+        {
+            $pontuacao_total[$piloto] = $pontuacao_atual[$piloto];
+        }
+
+        arsort($pontuacao_total);
+
+        $max_pontos = reset($pontuacao_total);
+        $campeao = key($pontuacao_total);
+
+        if ($pontuacao_total[$campeao] == end($pontuacao_total)) 
+        {
+            $campeoes[] = $campeao;
+        }
+    }
+}
+
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="pt">
 <head>
-    <title>Calculadora do Campeão Mundial de Pilotos da Fórmula 1</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Campeão Mundial de Pilotos</title>
 </head>
 <body>
-    <h1>Calculadora do Campeão Mundial de Pilotos da Fórmula 1</h1>
-
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-        <label for="grandes-premios">Número de Grandes Prêmios:</label>
-        <input type="number" id="grandes-premios" name="grandes-premios" required><br>
-
-        <label for="pilotos">Número de Pilotos:</label>
-        <input type="number" id="pilotos" name="pilotos" required><br>
+    <h1>Campeão Mundial de Pilotos</h1>
+    <form method="post">
+        <label for="G">Número de Grandes Prêmios:</label>
+        <input type="number" name="G" id="G" required>
+        <br>
+        <label for="P">Número de Pilotos:</label>
+        <input type="number" name="P" id="P" required>
+        <br>
 
         <?php
-        // Verifica se o formulário foi enviado
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $grandesPremios = $_POST["grandes-premios"];
-            $pilotos = $_POST["pilotos"];
-
-            echo "<h2>Insira os resultados das corridas:</h2>";
-
-            // Loop para inserir os resultados das corridas
-            for ($i = 1; $i <= $grandesPremios; $i++) {
-                echo "<h3>Grande Prêmio $i:</h3>";
-                for ($j = 1; $j <= $pilotos; $j++) {
-                    echo "Posição do Piloto $j no Grande Prêmio $i: <input type='number' name='corrida-$i-piloto-$j' required><br>";
-                }
+        for ($i = 0; $i < $G; $i++) 
+        {
+            echo "<h2>Resultado da Corrida " . ($i + 1) . "</h2>";
+            for ($j = 1; $j <= $P; $j++) 
+            {
+                echo "Posição do Piloto " . $j . ": ";
+                echo "<input type='number' name='corrida$i" . "[]' required>";
+                echo "<br>";
             }
         }
         ?>
 
-        <input type='submit' value='Calcular Campeão'>
+        <label for="S">Número de Sistemas de Pontuação:</label>
+        <input type="number" name="S" id="S" required>
+        <br>
+
+        <?php
+        for ($sistema = 0; $sistema < $S; $sistema++) 
+        {
+            echo "<h2>Sistema de Pontuação " . ($sistema + 1) . "</h2>";
+            echo "Último piloto que recebe pontos: ";
+            echo "<input type='number' name='K$sistema' required>";
+            echo "<br>";
+
+            for ($j = 1; $j <= $K; $j++) 
+            {
+                echo "Pontos para o Piloto " . $j . ": ";
+                echo "<input type='number' name='pontos$sistema" . "[]' required>";
+                echo "<br>";
+            }
+        }
+        ?>
+
+        <input type="submit" value="Calcular Campeão">
     </form>
 
     <?php
-    // Verifica se o formulário foi enviado
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Processamento dos resultados para determinar o campeão
-        $melhorPontuacao = 0;
-        $campeoes = [];
-
-        // Loop para calcular a pontuação de cada piloto
-        for ($p = 1; $p <= $pilotos; $p++) {
-            $pontuacao = 0;
-
-            for ($g = 1; $g <= $grandesPremios; $g++) {
-                $posicao = $_POST["corrida-$g-piloto-$p"];
-
-                // Implemente aqui o cálculo da pontuação com base na posição da corrida e no sistema de pontuação desejado
-                // Por exemplo, você pode usar a função calcularPontuacao($posicao, $g) para calcular a pontuação
-
-                // Aqui você pode adicionar diferentes sistemas de pontuação, como 2010 ou 2003
-                $pontuacao += calcularPontuacao($posicao, $g);
-            }
-
-            // Verifica se este piloto é o novo campeão
-            if ($pontuacao > $melhorPontuacao) {
-                $melhorPontuacao = $pontuacao;
-                $campeoes = [$p];
-            } elseif ($pontuacao == $melhorPontuacao) {
-                $campeoes[] = $p;
-            }
+    if ($_SERVER["REQUEST_METHOD"] === "POST") 
+    {
+        if (count($campeoes) === 1) 
+        {
+            echo "<h2>Campeão Mundial de Pilotos:</h2>";
+        } else 
+        {
+            echo "<h2>Campeões Mundiais de Pilotos:</h2>";
         }
-
-        echo "<h2>Campeão Mundial de Pilotos:</h2>";
-        echo "<p>Piloto(s) Campeão(s): " . implode(", ", $campeoes) . "</p>";
-    }
-
-    // Função para calcular a pontuação com base na posição da corrida e no sistema de pontuação
-    function calcularPontuacao($posicao, $grandePremio) {
-        // Aqui você pode implementar diferentes sistemas de pontuação
-        // Por exemplo, o sistema de 2010 e 2003
-
-        // Sistema de Pontuação de 2010
-        $pontuacao2010 = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1];
-
-        // Sistema de Pontuação de 2003
-        $pontuacao2003 = [10, 8, 6, 5, 4, 3, 2, 1];
-
-        // Escolha qual sistema de pontuação usar com base no grande prêmio
-        if ($grandePremio <= 10) {
-            return $pontuacao2010[$posicao - 1];
-        } else {
-            return $pontuacao2003[$posicao - 1];
-        }
+        echo implode(" ", $campeoes);
     }
     ?>
 </body>
